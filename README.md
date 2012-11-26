642c4a9c098e24295d2b1295a76cbcc119712a04
llinares <lean8086@gmail.com>
2012-11-26 14:08:11 -0300

    v0.2

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Tool to manage a queue of tasks for browser games.
 
 ## Purpose
 When a browser game needs to set a queue of tasks to be executed in specific times, even if the game window was closed, Postpone does it for you.
+
+## Change log
+
+### v0.2
+* Expose the queue as a public member.
+* Execute the tasks passing the task date object as parameter.
+
+### v0.1
+* First commit
--- postpone.js
+++ postpone.js
@@ -1,7 +1,7 @@
 /*!
  * Postpone
  * Tool to manage a queue of tasks for browser games.
- * @version 0.1
+ * @version 0.2
  * @author Leandro Linares (@lean8086)
  */
 (function (window) {
@@ -14,23 +14,25 @@
 		Object = window.Object,
 		Date = window.Date,
 		JSON = window.JSON,
-		postpone = {},
+		postpone = {};
 
-		/**
-		 * Object that saves all the dates with its callbacks.
-		 * @type Object
-		 * @private
-		 * @example
-		 * {
-		 *     'Wed Sep 26 2012 17:00:00 GMT-0300 (ART)': function () {
-		 *         console.log('Task to be executed on September 26th.');
-		 *     },
-		 *     'Thu Nov 8 2012 23:30:00 GMT-0300 (ART)': function () {
-		 *         console.log('Task to be executed on November 8th.');
-		 *     }
-		 * }
-		 */
-		queue = JSON.parse(storage.getItem('postponeQueue')) || {};
+	/**
+	 * Object that saves all the dates with its callbacks.
+	 * @name set
+	 * @memberOf postpone
+	 * @type Object
+	 * @example
+	 * {
+	 *     'Wed Sep 26 2012 17:00:00 GMT-0300 (ART)': function () {
+	 *         console.log('Task to be executed on September 26th.');
+	 *     },
+	 *     'Thu Nov 8 2012 23:30:00 GMT-0300 (ART)': function (date) {
+	 *         console.log('Task to be executed on minute ' + date.getMinutes());
+	 *         // It logs "Task to be executed on minute 30"
+	 *     }
+	 * }
+	 */
+	postpone.queue = JSON.parse(storage.getItem('postponeQueue')) || {};
 
 	/**
 	 * Method that associates a task with the corresponding date string.
@@ -50,9 +52,9 @@
 		// than a Date object.
 		on = Date(on);
 		// Set the method to be executed into the date string key
-		queue[on] = callback;
+		postpone.queue[on] = callback;
 		// Save all the postpone queue into local storage
-		storage.setItem('postponeQueue', JSON.stringify(queue));
+		storage.setItem('postponeQueue', JSON.stringify(postpone.queue));
 	};
 
 	/**
@@ -64,18 +66,20 @@
 	 */
 	postpone.check = function () {
 		// Proceed only if there are at least ONE task into queue
-		if (Object.keys(queue).length === 0) { return false; }
+		if (Object.keys(postpone.queue).length === 0) { return false; }
 		// Create a new Date instance of today
 		var now = new Date(),
+			// The string saved as task key
+			key = Object.keys(postpone.queue)[0],
 			// Get the first task on queue
-			task = new Date(Object.keys(queue)[0]);
+			taskDate = new Date(key);
 		// Compare task time with the new instance of Date
 		// Continue only if it IS or WAS time to execute the task
-		if (task <= now) {
-			// Execute the task callback
-			queue[task]();
+		if (taskDate <= now) {
+			// Execute the task callback passing the date object as parameter
+			postpone.queue[key](taskDate);
 			// Forget about this task for future executions
-			delete queue[task];
+			delete postpone.queue[key];
 			// Look for more tasks that shoulda been launched (recursive)
 			postpone.check();
 		}
